b2c2736540045f4fbb61aca7cf3b22346fa8a937
Gitea <gitea@fake.local>
2019-10-20 12:57:01 +0800

    仅保留 diff 以简化多线程处理问题；

--- time/cached_clock.hpp
+++ time/cached_clock.hpp
@@ -2,18 +2,16 @@
 #define WCCS_TIME_CACHED_CLOCK_H
 
 #include <cstdint>
+#include <atomic>
 #include <chrono>
+#include <iostream>
 
 namespace wccs::time {
     class cached_clock {
     public:
         // 可选提供当前参照时间
-        explicit cached_clock(std::chrono::milliseconds current = std::chrono::milliseconds(0))
-        : cached_(current.count())
-        , steady_(std::chrono::steady_clock::now()) {
-            if(cached_ == 0) {
-                cached_ = std::chrono::duration_cast<std::chrono::milliseconds>( std::chrono::system_clock::now().time_since_epoch() ).count();
-            }
+        explicit cached_clock(std::chrono::milliseconds current) {
+            swap(current);
         }
         // 系统时钟 
         operator std::chrono::system_clock::time_point() const { 
@@ -22,17 +20,27 @@ namespace wccs::time {
         }
         // 时间戳：秒
         operator std::uint32_t() const {
-            return cached_ / 1000 +
-                std::chrono::duration_cast<std::chrono::seconds>(std::chrono::steady_clock::now() - steady_).count();
+            return std::chrono::duration_cast<std::chrono::seconds>(
+                    std::chrono::steady_clock::now().time_since_epoch()).count() + (diff_ / 1000);
         }
         // 时间戳：毫秒
         operator std::int64_t() const {
-            return cached_ +
-                std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - steady_).count();
+            std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(
+                std::chrono::steady_clock::now().time_since_epoch()).count() << "\n";
+            return std::chrono::duration_cast<std::chrono::milliseconds>(
+                std::chrono::steady_clock::now().time_since_epoch()).count() + diff_;
+        }
+
+        void swap(std::chrono::milliseconds current = std::chrono::milliseconds(0)) {
+            std::int64_t local = std::chrono::duration_cast<std::chrono::milliseconds>( std::chrono::steady_clock::now().time_since_epoch() ).count(),
+                server = std::chrono::duration_cast<std::chrono::milliseconds>( current ).count(),
+                diff = server - local;
+            std::cout << diff << "\n";
+            diff_.store(diff);
+            std::cout << diff_ << "\n";
         }
     private:
-        std::int64_t cached_; // 毫秒
-        std::chrono::steady_clock::time_point steady_;
+        std::atomic_int64_t diff_; // 毫秒
     };
 }
 
