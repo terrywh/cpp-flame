15fdfca34069c070a7a75c308fa0e429fb499838
Gitea <gitea@fake.local>
2019-10-20 13:03:36 +0800

    模板化，允许单线程状态使用原始类型；

--- time/cached_clock.hpp
+++ time/cached_clock.hpp
@@ -7,10 +7,12 @@
 #include <iostream>
 
 namespace wccs::time {
+    template <typename DIFF_TYPE = std::atomic_int64_t>
     class cached_clock {
     public:
         // 可选提供当前参照时间
-        explicit cached_clock(std::chrono::milliseconds current) {
+        explicit cached_clock(std::chrono::milliseconds current =
+            std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now().time_since_epoch())) {
             swap(current);
         }
         // 系统时钟 
@@ -25,22 +27,18 @@ namespace wccs::time {
         }
         // 时间戳：毫秒
         operator std::int64_t() const {
-            std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(
-                std::chrono::steady_clock::now().time_since_epoch()).count() << "\n";
             return std::chrono::duration_cast<std::chrono::milliseconds>(
                 std::chrono::steady_clock::now().time_since_epoch()).count() + diff_;
         }
 
         void swap(std::chrono::milliseconds current = std::chrono::milliseconds(0)) {
             std::int64_t local = std::chrono::duration_cast<std::chrono::milliseconds>( std::chrono::steady_clock::now().time_since_epoch() ).count(),
-                server = std::chrono::duration_cast<std::chrono::milliseconds>( current ).count(),
-                diff = server - local;
-            std::cout << diff << "\n";
-            diff_.store(diff);
-            std::cout << diff_ << "\n";
+                server = std::chrono::duration_cast<std::chrono::milliseconds>( current ).count();
+
+            diff_ = server - local;
         }
     private:
-        std::atomic_int64_t diff_; // 毫秒
+        DIFF_TYPE diff_; // 毫秒
     };
 }
 
